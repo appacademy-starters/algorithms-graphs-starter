df8a7a23bfffe126773b4779daf245f1c09922ec
Derek Jou <derekjou0@gmail.com>
2021-02-05 15:50:27 -0800

    initial commit, thursday

---
+++ practice/graph-practice.js
@@ -0,0 +1,20 @@
+const minSetVertices = (n, edges) => {
+
+};
+
+const findJudge = trust => {
+
+};
+
+const numIslands = grid => {
+
+};
+
+const ladderLength = (start, end, wordList) => {
+
+};
+
+const minEffort = heights => {
+
+};
+
---
+++ practice/graph.js
@@ -0,0 +1,45 @@
+// Implement your GraphNode class here
+class GraphNode {
+    constructor() {
+
+    }
+}
+
+// Implement the Graph class here
+class Graph {
+    constructor() {
+        this.adjacencyList = null; // Fill this out
+    }
+
+    // Implement the following methods
+    // You may to declare the arguments yourself where necessary
+    addVertex() {
+
+    }
+
+    addEdge() {
+
+    }
+
+    deleteVertex() {
+
+    }
+
+    deleteEdge() {
+
+    }
+
+    breadthFirstTraversal() {
+
+    }
+
+    depthFirstTraversal() {
+
+    }
+
+}
+
+module.exports = {
+    GraphNode,
+    Graph
+}
